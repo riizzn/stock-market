25f2130d2471342a891d7310dc77b41a3350eff8
Rez <rezhates@gmail.com>
2025-12-02 00:52:18 +0530

    updated  market overview widget

--- components/TradingViewWidget.tsx
+++ components/TradingViewWidget.tsx
@@ -21,24 +21,18 @@ const TradingViewWidget = ({
   const containerRef = useTradingViewWidget(scriptUrl, config, height);
 
   return (
-    <div
-      className={`tradingview-widget-container ${className}`}
-      ref={containerRef}
-      style={{ height: "100%", width: "100%" }}
-    >
+    <div className="w-full">
+      {title && (
+        <h3 className="font-semibold text-2xl text-gray-100 mb-5 ">{title}</h3>
+      )}
       <div
-        className="tradingview-widget-container__widget"
-        style={{ height: "calc(100% - 32px)", width: "100%" }}
-      ></div>
-      <div className="tradingview-widget-copyright">
-        <a
-          href="https://www.tradingview.com/symbols/NASDAQ-AAPL/"
-          rel="noopener nofollow"
-          target="_blank"
-        >
-          <span className="blue-text">AAPL stock chart</span>
-        </a>
-        <span className="trademark"> by TradingView</span>
+        className={`tradingview-widget-container ${className}`}
+        ref={containerRef}
+      >
+        <div
+          className="tradingview-widget-container__widget"
+          style={{ height, width: "100%" }}
+        />
       </div>
     </div>
   );
