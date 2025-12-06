b048c7da37d608c73628c164d769266db17fdecc
Rez <rezhates@gmail.com>
2025-12-06 18:29:16 +0530

    Add  stocks search

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -2,6 +2,7 @@
 import { NAV_ITEMS } from "@/lib/Constants";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import SearchCommand from "./SearchCommand";
 
 const NavItems = () => {
   const pathname = usePathname();
@@ -12,20 +13,33 @@ const NavItems = () => {
   };
   return (
     <ul className="flex flex-col sm:flex-row gap-3 sm:gap-8 px-2  py-1.5 font-medium">
-      {NAV_ITEMS.map(({ href, title }) => (
-        <li key={href}>
-          <Link
-            href={href}
-            className={`hover:text-lilac-primary transition-colors ${
-              isActive(href) ? "text-gray-100" : ""
-            }
+      {NAV_ITEMS.map(({ href, title }) => {
+        if (title === "Search")
+          return (
+            <li key="search-trigger">
+              <SearchCommand
+                renderAs="text"
+                label="Search"
+                initialStocks={[]}
+              />
+            </li>
+          );
+
+        return (
+          <li key={href}>
+            <Link
+              href={href}
+              className={`hover:text-lilac-primary transition-colors ${
+                isActive(href) ? "text-gray-100" : ""
+              }
                 }`}
-          >
-            {" "}
-            {title}
-          </Link>
-        </li>
-      ))}
+            >
+              {" "}
+              {title}
+            </Link>
+          </li>
+        );
+      })}
     </ul>
   );
 };
